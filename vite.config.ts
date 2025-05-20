19186ed911d7292b4bc67efc86ed651dfc7d4251
gkdms0605 <leehaeun060523@gmail.com>
2025-05-20 09:07:58 +0900

    deploy : root fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  base: './',
   build: {
     outDir: 'y'
   }
